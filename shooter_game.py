17353596e4045069f86b1924f4725e03705a522f
Dark-Knight2011 <tanksblitz1811@mail.ru>
2025-04-26 14:01:10 +0300

    Add files via upload
    pinga Ponga v1.0

---
+++ shooter_game.py
@@ -0,0 +1,126 @@
+from pygame import *
+import random as rndm
+import time as tm
+
+
+mixer.init()
+font.init()
+
+window = display.set_mode((700, 500))
+display.set_caption('Догонялки')
+
+bg = 
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed=10, w = 65, h = 65):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        if keys_pressed[K_RIGHT] and self.ret.x < 600:
+            self.rect.x += self.speed
+        if keys_pressed[K_d] and self.rect.x < 600:
+            self.rect.x += self.speed
+        if keys_pressed[K_LEFT] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys_pressed[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y <= -50:
+            self.kill()
+
+class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed=3):
+        super().__init__(player_image, player_x, player_y, player_speed)
+        self.border_up = -50
+        self.border_down = 750
+    
+    def update(self):
+        global lost
+        global text_lost_ufos
+
+        self.rect.y += self.speed
+        if self.rect.y >= self.border_down:
+            self.speed = rndm.randint(1, 3)
+            self.rect.y = -50
+            self.rect.x = rndm.randint(30, 620)
+            lost += 1
+        text_lost_ufos = font1.render(
+            'Пропущено ' + str(lost), 1, (255, 255, 255)
+        )
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Meteorituzzzzzzz(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed=3):
+        super().__init__(player_image, player_x, player_y, player_speed)
+        self.border_up = -50
+        self.border_down = 850
+    
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y >= self.border_down:
+            self.speed = rndm.randint(1, 3)
+            self.rect.y = -50
+            self.rect.x = rndm.randint(30, 620)
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+lose_replic = ['Игрок был уничтожен', 'Игрок был низложен']
+win_r3pl1c = ['Игрок ecть победист!']
+
+
+lost = 0
+
+ded = 0
+
+# kick.play()
+
+FPS = 120
+
+fps = FPS*2
+
+fire_time = FPS/10
+
+font1 = font.SysFont('Arial', 36)
+
+enemy_z_ = []
+
+
+text_lost_ufos = font1.render(
+    'Пропущено: ' + str(lost), 1, (255, 255, 255)
+)
+
+text_ded_ufos = font1.render(
+    'Заанигилировано: ' + str(ded), 1, (255, 255, 255)
+)
+
+
+game = True
+
+game_process = True
+
+while game:
+    keys_pressed = key.get_pressed()
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
